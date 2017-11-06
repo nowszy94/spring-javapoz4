a603e727504cba6cec827c2b60785d8f5ecd6e2c
Szymon Nowak <szymon.nowak.sn1@contractors.roche.com>
2017-11-06 21:03:10 +0100

    controller with thymeleaf example

--- src/main/java/com/sda/springjavapoz4/controller/UsersController.java
+++ src/main/java/com/sda/springjavapoz4/controller/UsersController.java
@@ -1,10 +1,13 @@
 package com.sda.springjavapoz4.controller;
 
+import com.sda.springjavapoz4.model.User;
 import com.sda.springjavapoz4.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,6 +16,32 @@ public class UsersController {
 
     @Autowired
     private UsersService usersService;
+
+    @GetMapping("/{id}")
+    public ModelAndView getUser(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView("user");
+        User user = usersService.getUser(id);
+        modelAndView.addObject("user", user);
+        return modelAndView;
+    }
+
+    @GetMapping(params = {"lastName"})
+    public ModelAndView getUsersByLastName(@RequestParam("lastName") String lastName) {
+        ModelAndView modelAndView = new ModelAndView("users");
+        modelAndView.addObject("lastName", lastName);
+        return modelAndView;
+    }
+
+    @GetMapping(params = {"firstName"})
+    public ModelAndView getUsersByFirstName(@RequestParam("firstName") String firstName) {
+        ModelAndView modelAndView = new ModelAndView("users");
+        modelAndView.addObject("users", usersService.getUsersByFirstName(firstName));
+        return modelAndView;
+    }
+    //1. nowa metoda na sciezke /users
+    //2. parametr firstName
+    //3. W UsersService piszemy metode getUsersByFirstName -> List<User>
+    //4. System.out.println(lista) albo th:each(sprawdzcie na stackoverflow)
 
     @GetMapping("/example")
     public ModelAndView getExampleUser() {
--- src/main/java/com/sda/springjavapoz4/service/UsersService.java
+++ src/main/java/com/sda/springjavapoz4/service/UsersService.java
@@ -6,14 +6,42 @@ import com.sda.springjavapoz4.service.generator.LastNameGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class UsersService {
+
+    private List<User> users;
 
     @Autowired
     private FirstNameGenerator firstNameGenerator;
 
     @Autowired
     private LastNameGenerator lastNameGenerator;
+
+    public UsersService() {
+        this.users = new ArrayList<>();
+    }
+
+    @PostConstruct
+    public void init() {
+        this.users.add(getExampleUser());
+        this.users.add(getExampleUser());
+        this.users.add(getExampleUser());
+        this.users.add(getExampleUser());
+        this.users.add(getExampleUser());
+        this.users.add(getExampleUser());
+
+        users.forEach(user -> System.out.println(user));
+//        users.forEach(System.out::println);
+    }
+
+    public User getUser(int id) {
+        return id >= users.size() ? null : users.get(id);
+    }
 
     public User getExampleUser() {
         User user = new User();
@@ -22,5 +50,11 @@ public class UsersService {
         user.setLastName(lastNameGenerator.getRandomLastName());
         user.setPhoneNumber("12368643");
         return user;
+    }
+
+    public List<User> getUsersByFirstName(String firstName) {
+        return users.stream()
+                .filter(user -> user.getFirstName().equals(firstName))
+                .collect(Collectors.toList());
     }
 }
